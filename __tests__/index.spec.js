7bad73cbe396dc1c26c003a38ec7723314ca790b
afeiship <1290657123@qq.com>
2024-01-30 19:36:37 +0800

    refactor: use new imp

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,64 +1,62 @@
-(function () {
-  const NxJson2base64 = require('../src');
+const NxJson2base64 = require('../src');
 
-  describe('NxJson2base64 basic api test', function () {
-    test('test stringify:', function () {
-      var obj = {
-        name: 'afei',
-        age: 108
-      };
-      var res = 'eyJuYW1lIjoiYWZlaSIsImFnZSI6MTA4fQ==';
-      expect(res).toBe(NxJson2base64.encode(obj));
-    });
+describe('NxJson2base64 basic api test', function () {
+  test('test stringify:', function () {
+    var obj = {
+      name: 'afei',
+      age: 108
+    };
+    var res = 'eyJuYW1lIjoiYWZlaSIsImFnZSI6MTA4fQ==';
+    expect(res).toBe(NxJson2base64.encode(obj));
+  });
 
-    test('test parse:', function () {
-      var obj = {
-        name: 'afei',
-        age: 108
-      };
-      var res = 'eyJuYW1lIjoiYWZlaSIsImFnZSI6MTA4fQ==';
-      expect(obj).toEqual(NxJson2base64.decode(res));
-    });
+  test('test parse:', function () {
+    var obj = {
+      name: 'afei',
+      age: 108
+    };
+    var res = 'eyJuYW1lIjoiYWZlaSIsImFnZSI6MTA4fQ==';
+    expect(obj).toEqual(NxJson2base64.decode(res));
+  });
 
-    test('test str Chinese:', function () {
-      var obj = {
-        name: '一个中文文件名'
-      };
-      var res = 'eyJuYW1lIjoi5LiA5Liq5Lit5paH5paH5Lu25ZCNIn0=';
-      expect(obj).toEqual(NxJson2base64.decode(res));
-    });
+  test('test str Chinese:', function () {
+    var obj = {
+      name: '一个中文文件名'
+    };
+    var res = 'eyJuYW1lIjoi5LiA5Liq5Lit5paH5paH5Lu25ZCNIn0=';
+    expect(obj).toEqual(NxJson2base64.decode(res));
+  });
 
-    test('url encode case:', () => {
-      var data = {
-        url: 'http://localhost:3000/signin',
-        params: {
-          id: 123,
-          lang: 'zh_CN'
-        }
-      };
+  test('url encode case:', () => {
+    var data = {
+      url: 'http://localhost:3000/signin',
+      params: {
+        id: 123,
+        lang: 'zh_CN'
+      }
+    };
 
-      expect(NxJson2base64.encode(data)).toBe(
-        'eyJ1cmwiOiJodHRwOi8vbG9jYWxob3N0OjMwMDAvc2lnbmluIiwicGFyYW1zIjp7ImlkIjoxMjMsImxhbmciOiJ6aF9DTiJ9fQ=='
-      );
-    });
+    expect(NxJson2base64.encode(data)).toBe(
+      'eyJ1cmwiOiJodHRwOi8vbG9jYWxob3N0OjMwMDAvc2lnbmluIiwicGFyYW1zIjp7ImlkIjoxMjMsImxhbmciOiJ6aF9DTiJ9fQ=='
+    );
+  });
 
-    test('url decode case:', () => {
-      var str =
-        'eyJ1cmwiOiJodHRwOi8vbG9jYWxob3N0OjMwMDAvc2lnbmluIiwicGFyYW1zIjp7ImlkIjoxMjMsImxhbmciOiJ6aF9DTiJ9fQ==';
-      expect(NxJson2base64.decode(str)).toEqual({
-        url: 'http://localhost:3000/signin',
-        params: {
-          id: 123,
-          lang: 'zh_CN'
-        }
-      });
-    });
-
-    test('base64 should at NxJson2base64', () => {
-      const b64 = NxJson2base64.base64;
-      expect(b64).toBeDefined();
-      expect(b64.encode('afei')).toBe('YWZlaQ==');
-      expect(b64.decode('YWZlaQ==')).toBe('afei');
+  test('url decode case:', () => {
+    var str =
+      'eyJ1cmwiOiJodHRwOi8vbG9jYWxob3N0OjMwMDAvc2lnbmluIiwicGFyYW1zIjp7ImlkIjoxMjMsImxhbmciOiJ6aF9DTiJ9fQ==';
+    expect(NxJson2base64.decode(str)).toEqual({
+      url: 'http://localhost:3000/signin',
+      params: {
+        id: 123,
+        lang: 'zh_CN'
+      }
     });
   });
-})();
+
+  test('base64 should at NxJson2base64', () => {
+    const b64 = NxJson2base64.base64;
+    expect(b64).toBeDefined();
+    expect(b64.encode('afei')).toBe('YWZlaQ==');
+    expect(b64.decode('YWZlaQ==')).toBe('afei');
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
-(function () {
-  var global = typeof window !== 'undefined' ? window : this || Function('return this')();
-  var nx = global.nx || require('@jswork/next');
-  var base64 = global.Base64 || require('js-base64').Base64;
+import nx from '@jswork/next';
+import base64 from 'js-base64';
 
-  var NxJson2base64 = nx.declare('nx.Json2base64', {
-    statics: {
-      base64: base64,
-      encode: function (inObject) {
-        var str = JSON.stringify(inObject);
-        return base64.encode(str);
-      },
-      decode: function (inString) {
-        var jsonStr = base64.decode(inString);
-        return JSON.parse(jsonStr);
-      },
-      encodeURI: function (inObject) {
-        var str = this.encode(inObject);
-        return encodeURIComponent(str);
-      },
-      decodeURI: function (inString) {
-        var str = decodeURIComponent(inString);
-        return this.decode(str);
-      }
+const NxJson2base64 = nx.declare('nx.Json2base64', {
+  statics: {
+    base64,
+    encode: function (inObject) {
+      var str = JSON.stringify(inObject);
+      return base64.encode(str);
+    },
+    decode: function (inString) {
+      var jsonStr = base64.decode(inString);
+      return JSON.parse(jsonStr);
+    },
+    encodeURI: function (inObject) {
+      var str = this.encode(inObject);
+      return encodeURIComponent(str);
+    },
+    decodeURI: function (inString) {
+      var str = decodeURIComponent(inString);
+      return this.decode(str);
     }
-  });
-
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = NxJson2base64;
   }
-})();
+});
+
+if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
+  module.exports = NxJson2base64;
+}
+
+export default NxJson2base64;
